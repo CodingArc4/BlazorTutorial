2e1b794cd8dbfbee96512afd9004f1b8b220d4b3
CodingArc4 <kmunib10@outlook.com>
2023-12-19 17:31:59 +0500

    Add project files.

---
+++ BlazorServer/Data/BudgetBookService.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorServer.Data
+{
+    public class BudgetBookService
+    {
+        private static List<Entry> mockDb = new List<Entry>();
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            mockDb.Add(newEntry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+    }
+}
---
+++ BlazorServer/Data/Entry.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServer.Data
+{
+    public class Entry
+    {
+        public double Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
